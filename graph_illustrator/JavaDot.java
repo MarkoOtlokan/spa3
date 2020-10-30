673e8f9101e048609fb6b07cecd5643840bdfd8e
MarkoOtlokan <otlokanmarko@gmail.com>
2020-10-30 11:49:10 +0100

    matrix

--- graph_illustrator/JavaDot.java
+++ graph_illustrator/JavaDot.java
@@ -17,4 +17,25 @@ public class JavaDot{
       e.printStackTrace();
     }
   }
+
+  public static void writeFromMatrix(boolean[][] matrix, int size){
+    try{
+      FileWriter myWriter = new FileWriter("Test.dot");
+      myWriter.write("graph {");
+      //myWriter.write("A -- B B -- C C -- A C -- C ");
+      for(int i = 0; i < size; i++){
+        for(int j = 0; j < size; j++){
+          if(matrix[i][j]){
+            myWriter.write((i+1)+" -- "+(j+1));
+          }
+        }
+      }
+      myWriter.write("}");
+      myWriter.close();
+      System.out.println("Successfully wrote to the file.");
+    }catch(IOException e){
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+  }
 }
