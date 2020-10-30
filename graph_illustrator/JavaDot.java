a96cd8e96ee2b76396ab26d8bc0760f3c6244c83
MarkoOtlokan <otlokanmarko@gmail.com>
2020-10-30 11:59:58 +0100

    matrix

--- graph_illustrator/JavaDot.java
+++ graph_illustrator/JavaDot.java
@@ -26,7 +26,7 @@ public class JavaDot{
       for(int i = 0; i < size; i++){
         for(int j = 0; j < size; j++){
           if(matrix[i][j]){
-            myWriter.write((i+1)+" -- "+(j+1));
+            myWriter.write((i+1)+" -- "+(j+1)+"\n");
           }
         }
       }
