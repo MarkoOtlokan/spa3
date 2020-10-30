42580b60725b1f6e9c3931bac95127266a4fed59
MarkoOtlokan <otlokanmarko@gmail.com>
2020-10-30 10:41:08 +0100

    my

--- graph_illustrator/JavaDot.java
+++ graph_illustrator/JavaDot.java
@@ -7,7 +7,9 @@ public class JavaDot{
   public static void write(String[] args) {
     try {
       FileWriter myWriter = new FileWriter("Test.dot");
-      myWriter.write("graph { A -- B B -- C C -- A C -- C } ");
+      myWriter.write("graph {");
+      myWriter.write("A -- B B -- C C -- A C -- C ");
+      myWriter.write("}");
       myWriter.close();
       System.out.println("Successfully wrote to the file.");
     } catch (IOException e) {
