33a8e6706acd3ed00c5456f74c5a66796c3130e7
MarkoOtlokan <otlokanmarko@gmail.com>
2020-12-27 20:10:56 +0100

    3

---
+++ stabla/Main.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+  public static void main(String[] args) {
+    Mapa<String, String> m = new Mapa<String, String>();
+    try {
+      File myObj = new File("kola2s.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        String[] pod = data.split(";");
+    //    System.out.println(data);
+        m.add(pod[0], pod[1]);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+    m.maxDubina();
+    m.minDubina();
+    m.minVrednost();
+    m.maxVrednost();
+    m.rebalansiraj();
+    m.maxDubina();
+    m.minDubina();
+    m.minVrednost();
+    m.maxVrednost();
+  //  m.printNode(m.koren);
+  }
+
+
+}
---
+++ stabla/Mapa.java
@@ -0,0 +1,275 @@
+import java.util.*;
+public class Mapa<K extends Comparable<K>, V>{
+  public Cvor koren = null;
+  public int velicina = 0;
+
+
+  public class Cvor{
+    K kljuc;
+    V vrednost;
+    Cvor levi, desni;
+
+    Cvor(K kljuc, V vrednost){
+      this.kljuc = kljuc;
+      this.vrednost = vrednost;
+    }
+
+    public String toString(){
+      return (String)kljuc;
+    }
+  }
+
+  class komparator implements Comparator<Cvor>{
+    public int compare(Cvor a, Cvor b)
+    {
+        return a.kljuc.compareTo(b.kljuc);
+    }
+  }
+
+  public void add(K kljuc, V vrednost){
+    if(koren == null){
+      koren = new Cvor(kljuc, vrednost);
+    }
+    addCvor(koren, kljuc, vrednost);
+    velicina++;
+  }
+
+  public void addCvor(Cvor c, K kljuc, V vrednost){
+  //  System.out.println(c.kljuc);
+    if(c.kljuc == kljuc){
+      c.vrednost = vrednost;
+      return;
+    }
+    if(c.kljuc.compareTo(kljuc) > 0){
+      if(c.desni == null){
+        c.desni = new Cvor(kljuc, vrednost);
+        return;
+      }
+      addCvor(c.desni, kljuc, vrednost);
+    }
+    else{
+      if(c.levi == null){
+        c.levi = new Cvor(kljuc, vrednost);
+        return;
+      }
+      addCvor(c.levi, kljuc, vrednost);
+      return;
+    }
+
+  }
+
+  public void maxDubina(){
+    System.out.println("Maximalna dubina je "+maxDubina(koren));
+  }
+
+  public int maxDubina(Cvor c){
+    if(c == null)
+      return 0;
+  //  System.out.println(c.kljuc);
+    return Math.max(maxDubina(c.levi)+1, maxDubina(c.desni)+1);
+  }
+
+  public void minDubina(){
+    System.out.println("Minimalna dubina je "+minDubina(koren));
+  }
+
+  public int minDubina(Cvor c){
+    if(c == null)
+      return 0;
+  //  System.out.println(c.kljuc);
+    return Math.min(minDubina(c.levi)+1, minDubina(c.desni)+1);
+  }
+
+  public void minVrednost(){
+    System.out.println("Minimalna vrednost je "+minVrednost(koren));
+  }
+
+  public K minVrednost(Cvor c){
+    if(c == null)
+      return null;
+    if(c.desni == null)
+      return c.kljuc;
+    return minVrednost(c.desni);
+  }
+
+  public void maxVrednost(){
+    System.out.println("Maximalna vrednost je "+maxVrednost(koren));
+  }
+
+  public K maxVrednost(Cvor c){
+    if(c == null)
+      return null;
+    if(c.levi == null)
+      return c.kljuc;
+    return maxVrednost(c.levi);
+  }
+
+  public void rebalansiraj(){
+    ArrayList<Cvor> niz = vratiNiz();
+    System.out.println(niz);
+    Collections.sort(niz, new komparator());
+    System.out.println(niz);
+    koren = null;
+    rebalansiraj(niz, 0, niz.size());
+  }
+  private void rebalansiraj(List<Cvor> niz, int i, int j){
+    int polovina = (i+j)/2;
+  //  System.out.println(niz);
+    //System.out.println(i+"  "+j);
+    add(niz.get(polovina).kljuc, niz.get(polovina).vrednost);
+    if(polovina > i){
+      rebalansiraj(niz, i, polovina);
+    }
+    if(polovina < j-1){
+      rebalansiraj(niz, polovina, j);
+    }
+
+  }
+
+  public ArrayList<Cvor> vratiNiz(){
+    ArrayList<Cvor> niz = new ArrayList<Cvor>();
+    //int brojac = 0;
+    Queue<Cvor> red = new LinkedList<Cvor>();
+    red.add(koren);
+    while(red.size() != 0){
+      Cvor trenutni = red.poll();
+      niz.add(trenutni);
+      if(trenutni.levi != null)
+        red.add(trenutni.levi);
+      if(trenutni.desni != null)
+        red.add(trenutni.desni);
+    }
+    return niz;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  public  <T extends Comparable<?>> void printNode(Cvor root) {
+     int maxLevel = maxLevel(root);
+
+     printNodeInternal(Collections.singletonList(root), 1, maxLevel);
+ }
+
+ private  <T extends Comparable<?>> void printNodeInternal(List<Cvor> nodes, int level, int maxLevel) {
+     if (nodes.isEmpty() || isAllElementsNull(nodes))
+         return;
+
+     int floor = maxLevel - level;
+     int endgeLines = (int) Math.pow(2, (Math.max(floor - 1, 0)));
+     int firstSpaces = (int) Math.pow(2, (floor)) - 1;
+     int betweenSpaces = (int) Math.pow(2, (floor + 1)) - 1;
+
+     printWhitespaces(firstSpaces);
+
+     List<Cvor> newNodes = new ArrayList<Cvor>();
+     for (Cvor node : nodes) {
+         if (node != null) {
+             System.out.print(node.kljuc);
+             newNodes.add(node.levi);
+             newNodes.add(node.desni);
+         } else {
+             newNodes.add(null);
+             newNodes.add(null);
+             System.out.print(" ");
+         }
+
+         printWhitespaces(betweenSpaces);
+     }
+     System.out.println("");
+
+     for (int i = 1; i <= endgeLines; i++) {
+         for (int j = 0; j < nodes.size(); j++) {
+             printWhitespaces(firstSpaces - i);
+             if (nodes.get(j) == null) {
+                 printWhitespaces(endgeLines + endgeLines + i + 1);
+                 continue;
+             }
+
+             if (nodes.get(j).levi != null)
+                 System.out.print("/");
+             else
+                 printWhitespaces(1);
+
+             printWhitespaces(i + i - 1+5);
+
+             if (nodes.get(j).desni != null)
+                 System.out.print("\\");
+             else
+                 printWhitespaces(1);
+
+             printWhitespaces(endgeLines + endgeLines - i);
+         }
+
+         System.out.println("");
+     }
+
+     printNodeInternal(newNodes, level + 1, maxLevel);
+ }
+
+ private  void printWhitespaces(int count) {
+     for (int i = 0; i < count; i++)
+         System.out.print(" ");
+ }
+
+ private  <T extends Comparable<?>> int maxLevel(Cvor node) {
+     if (node == null)
+         return 0;
+
+     return Math.max(maxLevel(node.levi), maxLevel(node.desni)) + 1;
+ }
+
+ private  <T> boolean isAllElementsNull(List<T> list) {
+     for (Object object : list) {
+         if (object != null)
+             return false;
+     }
+
+     return true;
+ }
+
+}
